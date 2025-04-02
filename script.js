2cb945b25407b38a006227fe50a34c747420d226
Sibisiddharth5 <145826914+Sibisiddharth5@users.noreply.github.com>
2025-04-03 01:35:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let display = document.getElementById("display");
+
+function appendNumber(num) {
+    display.value += num;
+}
+
+function appendOperator(op) {
+    if (display.value !== "" && !isNaN(display.value.slice(-1))) {
+        display.value += op;
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function toggleSign() {
+    if (display.value) {
+        display.value = parseFloat(display.value) * -1;
+    }
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
